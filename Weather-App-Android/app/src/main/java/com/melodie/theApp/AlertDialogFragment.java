e9d81a97f552f9abd0dfba63c627ca19d63aa05f
Melodie21 <tchitchou50@hotmail.fr>
2017-04-30 16:21:34 +0200

    Update AlertDialogFragment.java

--- Weather-App-Android/app/src/main/java/com/melodie/theApp/AlertDialogFragment.java
+++ Weather-App-Android/app/src/main/java/com/melodie/theApp/AlertDialogFragment.java
@@ -1,4 +1,4 @@
-package com.allentbrown.stormy;
+package com.melodie.stormy;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
@@ -6,9 +6,7 @@ import android.app.DialogFragment;
 import android.content.Context;
 import android.os.Bundle;
 
-/**
- * Created by abrown54 on 7/8/2015.
- */
+
 public class AlertDialogFragment extends DialogFragment {
 
     @Override
